171e5a8922e38bdc3323424617798e440b3bdabd
Florb11 <florbergman11@gmail.com>
2024-10-03 11:54:15 -0300

    2

--- TrabajoPOO/src/Ciudades.java
+++ TrabajoPOO/src/Ciudades.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Ciudades {
     //atributos
     private String nombre;
@@ -6,5 +8,42 @@ public class Ciudades {
 
     // constructor
 
+    public Ciudades(String nombre, String[][] temperaturas, int cantidad) {
+        this.nombre = nombre;
+        this.temperaturas = temperaturas;
+        this.cantidad = cantidad;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String[][] getTemperaturas() {
+        return temperaturas;
+    }
+
+    public void setTemperaturas(String[][] temperaturas) {
+        this.temperaturas = temperaturas;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciudades{" +
+                "nombre='" + nombre + '\'' +
+                ", temperaturas=" + Arrays.toString(temperaturas) +
+                ", cantidad=" + cantidad +
+                '}';
+    }
 }
